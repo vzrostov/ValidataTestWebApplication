cc163085d775cc14916551decc8b849fdae32521
Vadim <zolotilovv@gmail.com>
2022-04-22 00:47:15 +0300

    last migration

---
+++ ValidataTest.Core/Migrations/202204212146223_m1.cs
@@ -0,0 +1,75 @@
+﻿namespace ValidataTest.Core.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class m1 : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(nullable: false, maxLength: 100),
+                        LastName = c.String(nullable: false, maxLength: 100),
+                        Address = c.String(nullable: false, maxLength: 200),
+                        PostalCode = c.String(nullable: false, maxLength: 15),
+                    })
+                .PrimaryKey(t => t.CustomerID);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                        CustomerID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.Customers", t => t.CustomerID, cascadeDelete: true)
+                .Index(t => t.CustomerID);
+            
+            CreateTable(
+                "dbo.Items",
+                c => new
+                    {
+                        ItemId = c.Int(nullable: false, identity: true),
+                        Quantity = c.Single(nullable: false),
+                        ProductID = c.Int(nullable: false),
+                        OrderID = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ItemId)
+                .ForeignKey("dbo.Products", t => t.ProductID, cascadeDelete: true)
+                .ForeignKey("dbo.Orders", t => t.OrderID, cascadeDelete: true)
+                .Index(t => t.ProductID)
+                .Index(t => t.OrderID);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 200),
+                        Price = c.Single(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Orders", "CustomerID", "dbo.Customers");
+            DropForeignKey("dbo.Items", "OrderID", "dbo.Orders");
+            DropForeignKey("dbo.Items", "ProductID", "dbo.Products");
+            DropIndex("dbo.Items", new[] { "OrderID" });
+            DropIndex("dbo.Items", new[] { "ProductID" });
+            DropIndex("dbo.Orders", new[] { "CustomerID" });
+            DropTable("dbo.Products");
+            DropTable("dbo.Items");
+            DropTable("dbo.Orders");
+            DropTable("dbo.Customers");
+        }
+    }
+}
